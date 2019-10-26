8d0fa503c113534c854a9477a10eee33fd3edcdd
Evodio Garcia <evodiog@gmail.com>
2019-10-25 17:47:45 -0700

    a sample project for my personal portfolio

---
+++ README.md
@@ -0,0 +1,7 @@
+## Grid-Sample-Site
+- Uses CSS grid and flex-box to create a mockup site.
+- Basic HTML page served up by express
+
+``
+npm install
+``
---
+++ public/index.js
@@ -0,0 +1,115 @@
+const images = {
+  man: {
+    heading: 'Man Image',
+    description: 'Image of a man.',
+    path: './images/man.jpg'
+  },
+  apartment: {
+    heading: 'Apartment Image',
+    description: 'Woman working out in apartment.',
+    path: './images/apartment.jpg'
+  },
+  beast: {
+    heading: 'Beast Image',
+    description: 'Image of a beast.',
+    path: '../images/beast.jpg'
+  },
+  developer: {
+    heading: 'Image of Me',
+    description: 'A developer',
+    path: './images/profile.jpg'
+  },
+  wizard: {
+    heading: 'Wizard Image',
+    description: 'Image of a wizard.',
+    path: '../images/wizard.jpg'
+  },
+  computer: {
+    heading: 'Computer Image',
+    description: 'Image of a laptop computer.',
+    path: './images/computer.jpeg'
+  }
+};
+
+const createElement = ({
+  type = 'div',
+  attributes = {},
+  children = null
+}) => {
+  const container = document.createElement(type);
+
+  for (let attribute in attributes) {
+    container.setAttribute(attribute, attributes[attribute]);
+  }
+
+  if (children) {
+    container.innerHTML = children;
+  }
+  return container
+}
+
+const applyMiddleHoverElements = ({ description, heading }, imagesWrapper) => {
+  const textContainer = createElement({
+    type: 'div',
+    attributes: { class: 'middle-display' }
+  });
+
+  const h1Element = createElement({
+    type: 'h1',
+    children: heading
+  });
+
+  const h2Element = createElement({
+    type: 'h2',
+    children: description
+  });
+
+  imagesWrapper.appendChild(textContainer);
+  textContainer.appendChild(h1Element);
+  textContainer.appendChild(h2Element);
+};
+
+const appendImages = () => {
+  Object.values(images).map(image => {
+
+    const imagesWrapper = document.getElementById('images');
+
+    const containerElement = createElement({
+      type: 'div',
+      attributes: { class: 'image-container' }
+    });
+    const imageElement = createElement({
+      type: 'img', attributes: {
+        src: image.path,
+        alt: image.description,
+        title: image.heading
+      }
+    });
+
+    imagesWrapper.appendChild(containerElement);
+    containerElement.appendChild(imageElement);
+
+    applyMiddleHoverElements(image, containerElement);
+  });
+  return
+};
+
+const toggleDropdown = () => {
+  const dropdown = document.getElementsByClassName("dropdown-button");
+  let i;
+
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+}
+
+window.addEventListener('load', appendImages);
+window.addEventListener('load', toggleDropdown);
